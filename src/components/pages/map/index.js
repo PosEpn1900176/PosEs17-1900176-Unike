7e151ca21dcd52a1d7957ab4ed714b2fdf9b5ae9
Leandro Budau de Moraes <lbmoraes@uolinc.com>
2020-02-15 17:36:12 -0300

    Criando estrutura basica mapa scheduled

--- src/components/pages/map/index.js
+++ src/components/pages/map/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { Text } from 'native-base';
+import { Text, Container, Content, Footer, Button, Icon } from 'native-base';
+import { ShowMap } from '../../templates/showMap';
+import { HeaderSignup } from '../../templates/headerSignup';
 
 const MapPage = ({ navigation, route }) => {
-  console.log('route', route);
-  return <Text>sdsdsd</Text>;
+  const goBack = () => {
+    navigation.goBack();
+  };
+  return (
+    <Container>
+      <HeaderSignup />
+      <Content>
+        <ShowMap />
+      </Content>
+      <Footer>
+        <Button onPress={goBack}>
+          <Text>Voltar</Text>
+        </Button>
+      </Footer>
+    </Container>
+  );
 };
 
 export { MapPage };

90b6ff323ddf61f1a207c54816b0d01032bcee46
Leandro Budau de Moraes <lbmoraes@uolinc.com>
2020-03-24 00:14:02 -0300

    Preparando lista de serviço

--- src/components/templates/scheduleServices/presentation/tableSchedule.js
+++ src/components/templates/scheduleServices/presentation/tableSchedule.js
@@ -24,14 +24,60 @@ import { DataValue } from '../../../molecules';
 import { HeaderSignup } from '../../headerSignup';
 import { ListService } from '../../listService';
 import { getFullName, getFullAddress, getDate } from '../../../../lib';
-import { ScrollView, View } from 'react-native';
+import { ScrollView, View, FlatList } from 'react-native';
 
 const TableScheduleServicesPresentation = ({ data = [] }) => {
+  return (
+    <List>
+      <FlatList
+        data={data}
+        keyExtractor={item => item.Id}
+        renderItem={({ item }) => (
+          <ListItem>
+            <Body>
+              <View
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}>
+                <View style={{ flex: 1 }}>
+                  <Text>{getFullName(item.Cliente)}</Text>
+                  <Text note>{getFullAddress(item.EnderecoCliente)}</Text>
+                  <View style={{ flexDirection: 'row', padding: 5 }}>
+                    {item.ItemsPedido.map((request, index) => {
+                      return (
+                        <View key={index}>
+                          <Text note>{getDate(request.Horario)}</Text>
+                          <View style={{ padding: 5 }}>
+                            <Badge primary>
+                              <Text>{request.Servico.Nome}</Text>
+                            </Badge>
+                          </View>
+                        </View>
+                      );
+                    })}
+                  </View>
+                </View>
+                <View>
+                  <Button transparent>
+                    <Icon name="arrow-forward" />
+                  </Button>
+                </View>
+              </View>
+            </Body>
+          </ListItem>
+        )}
+      />
+    </List>
+  );
+};
+
+const TableScheduleServicesPresentation1 = ({ data = [] }) => {
   console.log('Presentation', data);
   return (
     <Container>
       <Content>
-        {/* <ScrollView> */}
         <List>
           {data.map((item, index) => {
             return (
@@ -72,99 +118,9 @@ const TableScheduleServicesPresentation = ({ data = [] }) => {
             );
           })}
         </List>
-        {/* </ScrollView> */}
       </Content>
     </Container>
   );
 };
 
 export default TableScheduleServicesPresentation;
-
-// export class TableScheduleServicesPresentation1 extends React.Component {
-//   render() {
-//     return (
-//       <>
-//         <DeckSwiper
-//           ref={c => (this._deckSwiper = c)}
-//           dataSource={this.props.data}
-//           renderItem={(item, index) => {
-//             console.log('ITEM', item);
-//             return (
-//               <Card key={index} style={styles.scheduleCard}>
-//                 <CardItem header>
-//                   <Left>
-//                     <Button
-//                       transparent
-//                       iconRight
-//                       onPress={() => this._deckSwiper._root.swipeRight()}>
-//                       <Icon type="AntDesign" name="left" />
-//                     </Button>
-//                   </Left>
-//                   <Body>
-//                     <Button
-//                       transparent
-//                       block
-//                       iconRight
-//                       onPress={() => this.props.onViewMap([item])}>
-//                       <Icon
-//                         type="EvilIcons"
-//                         name="location"
-//                         style={styles.iconLarge}
-//                       />
-//                     </Button>
-//                   </Body>
-//                   <Right>
-//                     <Button
-//                       transparent
-//                       iconRight
-//                       onPress={() => this._deckSwiper._root.swipeRight()}>
-//                       <Icon type="AntDesign" name="right" />
-//                     </Button>
-//                   </Right>
-//                 </CardItem>
-
-//                 <CardItem cardBody>
-//                   <Left>
-//                     <Body>
-//                       <DataValue label="Cliente" value={item.Cliente.Nome} />
-//                       <DataValue
-//                         label="Endereco"
-//                         value={item.EnderecoCliente.Endereco}
-//                       />
-//                       {/* <DataValue label="Serviço" value={item.Servico} /> */}
-//                       <DataValue label="Horário" value={item.HorarioInicial} />
-//                       {/* <DataValue
-//                         label="Taxa do serviço"
-//                         value={item.TaxaServico}
-//                       />
-//                       <DataValue
-//                         label="Valor do serviço"
-//                         value={item.ValorServico}
-//                       />
-//                       <DataValue label="Desconto" value={item.Desconto} /> */}
-//                       {/* <Grid>
-//                         <Col>
-//                           <DataValue
-//                             label="Total à receber"
-//                             value={item.TotalReceber}
-//                           />
-//                         </Col>
-//                         <Col>
-//                           <Button
-//                             primary
-//                             onPress={() => this.props.onFinish(item)}>
-//                             <Text>Fnalizar serviço</Text>
-//                           </Button>
-//                         </Col>
-//                       </Grid> */}
-//                     </Body>
-//                   </Left>
-//                 </CardItem>
-//               </Card>
-//             );
-//           }}
-//         />
-//       </>
-//     );
-//   }
-// }

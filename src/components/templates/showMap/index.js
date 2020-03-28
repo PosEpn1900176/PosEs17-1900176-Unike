dff702012a27a7d5d8785b89ba5b0bb0c1df7cda
Leandro Budau de Moraes <lbmoraes@uolinc.com>
2020-03-27 21:30:25 -0300

    Instalando dependencias para mostrar direçao no mapa

--- src/components/templates/showMap/index.js
+++ src/components/templates/showMap/index.js
@@ -5,11 +5,14 @@ import { useGetRegion } from '../../../hooks';
 import { useSelector } from 'react-redux';
 import styles from './styles';
 
+const GOOGLE_MAPS_APIKEY = 'AIzaSyA7yc3fZnlRQIL9iN51enydQ-Vnx--Cz24';
+
+import MapViewDirections from 'react-native-maps-directions';
+
 const ShowMap = props => {
   console.log('data', props);
   const { coords } = useSelector(state => state.services.map.currentPosition);
   const getRegion = useGetRegion();
-
   const onPress = marker => {
     return event => {
       props.onPressMarker(marker);

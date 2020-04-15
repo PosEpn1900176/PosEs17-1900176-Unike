c1f9860592a77fae6133fce6438f755df051b34b
PosEpn1900176 <eduardo.eduardonunes@gmail.com>
2020-04-15 14:40:36 -0300

    Pagina Sobre

--- src/components/pages/help/presentation/index.js
+++ src/components/pages/help/presentation/index.js
@@ -3,7 +3,7 @@ import { HeaderSignup } from '../../../templates/headerSignup';
 import { Container, Tabs, Tab, TabHeading, Footer, Left, Text, Button, Icon } from 'native-base';
 
 const HelpPagePresentation = props => {
-  const onBack = () => {
+    const onBack = () => {
     props.navigation.goBack();
   };
   return (

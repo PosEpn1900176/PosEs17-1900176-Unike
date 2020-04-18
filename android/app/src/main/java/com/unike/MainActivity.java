1154b3f38dfe43889916a91c2b2940c1d654c623
PosEpn1900176 <eduardo.eduardonunes@gmail.com>
2020-04-18 13:26:19 -0300

    Configurações para o Deploy e Alteração dos Icones de Imagem

--- android/app/src/main/java/com/unike/MainActivity.java
+++ android/app/src/main/java/com/unike/MainActivity.java
@@ -1,4 +1,4 @@
-package com.unike;
+package com.prfunike;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/unike/MainApplication.java
+++ android/app/src/main/java/com/unike/MainApplication.java
@@ -1,4 +1,4 @@
-package com.unike;
+package com.prfunike;
 
 import android.app.Application;
 import android.content.Context;
